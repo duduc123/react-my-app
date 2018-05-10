a641bbd753c27f3fd74baa65f0c41f55b978bd3c
duduc123 <duduc123@126.com>
2018-05-10 23:48:31 +0800

    modify head

--- app/containers/Head/head.js
+++ app/containers/Head/head.js
@@ -19,23 +19,50 @@ export default class Head extends React.Component {
     return (
       <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
         <Menu.Item key="mail">
-          <Icon type="mail"/>Navigation One
+          <Icon type="mail"/>主页
         </Menu.Item>
-        <Menu.Item key="app" disabled>
-          <Icon type="appstore"/>Navigation Two
-        </Menu.Item>
-        <SubMenu title={< span > <Icon type="setting"/>Navigation Three - Submenu </span>}>
+        <SubMenu title={< span > <Icon type="setting"/>个人业务</span>}>
           <MenuItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
+            <Menu.Item key="setting:1">金葵花理财</Menu.Item>
+            <Menu.Item key="setting:2">私人银行</Menu.Item>
           </MenuItemGroup>
           <MenuItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
+            <Menu.Item key="setting:3">出国金融</Menu.Item>
+            <Menu.Item key="setting:4">个人贷款</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <SubMenu title={< span > <Icon type="setting"/>公司业务</span>}>
+          <MenuItemGroup title="Item 1">
+            <Menu.Item key="setting:1">现金管理</Menu.Item>
+            <Menu.Item key="setting:2">国内业务</Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Item 2">
+            <Menu.Item key="setting:3">跨境金融</Menu.Item>
+            <Menu.Item key="setting:4">投资银行</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <SubMenu title={< span > <Icon type="setting"/>信用卡</span>}>
+          <MenuItemGroup title="Item 1">
+            <Menu.Item key="setting:1">信用卡首页</Menu.Item>
+            <Menu.Item key="setting:2">客户服务</Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Item 2">
+            <Menu.Item key="setting:3">信用卡在线申请</Menu.Item>
+            <Menu.Item key="setting:4">特惠快讯</Menu.Item>
+          </MenuItemGroup>
+        </SubMenu>
+        <SubMenu title={< span > <Icon type="setting"/>今日招行</span>}>
+          <MenuItemGroup title="Item 1">
+            <Menu.Item key="setting:1">招行新闻</Menu.Item>
+            <Menu.Item key="setting:2">招行介绍</Menu.Item>
+          </MenuItemGroup>
+          <MenuItemGroup title="Item 2">
+            <Menu.Item key="setting:3">投资者关系</Menu.Item>
+            <Menu.Item key="setting:4">微博</Menu.Item>
           </MenuItemGroup>
         </SubMenu>
         <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+          <a href="#" target="_blank" rel="noopener noreferrer">Travel Book</a>
         </Menu.Item>
       </Menu>
     );
